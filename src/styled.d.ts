cf41c4e1e4c0871e060f0efee3c5d980198f9619
DarkEye <lesko.matej.pu@gmail.com>
2020-08-30 11:10:56 +0200

    typing for teme

---
+++ src/styled.d.ts
@@ -0,0 +1,47 @@
+// import original module declarations
+import 'styled-components';
+
+// and extend them!
+declare module 'styled-components' {
+  export interface DefaultTheme {
+    color: {
+      darkShades: string;
+      darkAccent: string;
+      main: string;
+      lightAccent: string;
+      lightShades: string;
+    };
+    breakpoints: { sm: string; md: string; lg: string; xl: string };
+    fontWeights: {
+      normal: number;
+      medium: number;
+      bold: number;
+    };
+    fontSizes: {
+      xs: string;
+      sm: string;
+      md: string;
+      lg: string;
+      xl: string;
+      '2xl': string;
+      '3xl': string;
+      '4xl': string;
+      '5xl': string;
+      '6xl': string;
+    };
+    space: {
+      xs: string;
+      sm: string;
+      md: string;
+      lg: string;
+      xl: string;
+      xxl: string;
+    };
+    sizes: {
+      xs: string;
+      sm: string;
+      xl: string;
+      full: string;
+    };
+  }
+}
--- src/theme.ts
+++ src/theme.ts
@@ -1,43 +1,29 @@
+import { DefaultTheme } from 'styled-components';
+
 const space = {
-  px: '1px',
-  0: '0',
-  1: '0.25rem',
-  2: '0.5rem',
-  3: '0.75rem',
-  4: '1rem',
-  5: '1.25rem',
-  6: '1.5rem',
-  8: '2rem',
-  10: '2.5rem',
-  12: '3rem',
-  16: '4rem',
-  20: '5rem',
-  24: '6rem',
-  32: '8rem',
-  40: '10rem',
-  48: '12rem',
-  56: '14rem',
-  64: '16rem',
+  xs: '0.25rem',
+  sm: '0.5rem',
+  md: '1rem',
+  lg: '2rem',
+  xl: '4rem',
+  xxl: '5rem',
 };
 
 const sizes = {
-  ...space,
+  xs: '2rem',
+  sm: '4rem',
+  xl: '6.25rem',
   full: '100%',
-  '3xs': '14rem',
-  '2xs': '16rem',
-  xs: '20rem',
-  sm: '24rem',
-  md: '28rem',
-  lg: '32rem',
-  xl: '36rem',
-  '2xl': '42rem',
-  '3xl': '48rem',
-  '4xl': '56rem',
-  '5xl': '64rem',
-  '6xl': '72rem',
+  // '3xs': '14rem',
+  // '2xs': '16rem',
+  // xs: '20rem',
+  // sm: '24rem',
+  // md: '28rem',
+  // lg: '32rem',
+  // xl: '36rem',
 };
 
-const theme = {
+const theme: DefaultTheme = {
   color: {
     darkShades: '#32384C',
     darkAccent: '#798394',
